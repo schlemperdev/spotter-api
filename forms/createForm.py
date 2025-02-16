e47b95828bb60d45407c3c0078996311905401b1
Schlemper <schlemperdev@gmail.com>
2025-02-16 16:21:41 -0300

    reformulated data register form

---
+++ forms/createForm.py
@@ -0,0 +1,32 @@
+def lead_data_collect():
+    print("Cadastro de Lead:")
+    return {
+        "cpfCnpj": input("CPF/CNPJ: "),
+        "name": input("Nome do Lead: "),
+        "sdrEmail": "renan.schlemper@brasilrad.com.br",
+        "ddiPhone": "55",
+        "phone": input("Telefone: "),
+        "address": input("Endereço: "),
+        "addressNumber": input("Número: "),
+        "addressComplement": input("Complemento: "),
+        "neighborhood": input("Bairro: "),
+        "zipcode": input("CEP: "),
+        "city": input("Cidade: "),
+        "state": input("Estado: "),
+        "country": input("País: "),
+        "industry": input("Setor: "),
+        "source": input("Origem: "),
+        "subSource": input("Sub-origem: "),
+        "funnelId": input("ID do Funil: "),
+        "stage": input("Etapa: "),
+    }
+
+
+def contact_data_collect():
+    print("Cadastro de Contato:")
+    return {
+        "contactName": input("Nome do Contato: "),
+        "contactEmail": input("E-mail: "),
+        "ddiPhone1": "55",
+        "phone1": input("Telefone: ")
+    }
