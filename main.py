01c7363a2d9613d745abc0a2c1487cda6316687e
Renan <schlemperdev@gmail.com>
2025-02-17 10:32:32 -0300

    finished main loop for new structure - still testing

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 from forms.mainForm import data_collect, contact_data_collect
-from models.LeadModel import Lead
-from models.ContactModel import Contact
-from serializers import LeadSerializer, ContactSerializer
-from api_requests.crm_api import create_lead, create_contact
+from controllers.OrganizationController import handle_org
+from controllers.LeadController import handle_lead
+from controllers.ConctactController import handle_contact
 
-# Coletar dados do Lead
-dados_lead = data_collect()
-novo_lead = Lead(**dados_lead)
+def main():
+    print ('Iniciando Cadastro de Lead e Organização. Por favor digite os dados solicitados abaixo:\n')
+    main_data = data_collect()
 
-# Enviar Lead para API e obter leadId real
-lead_json = LeadSerializer.serialize(novo_lead)
-resposta = create_lead(lead_json)
-novo_lead.leadId = resposta["leadId"]
+    organization_id = handle_org(main_data)
+    if not organization_id:
+        print('Erro ao criar organização')
+        return
+    
+    lead_id = handle_lead(main_data, organization_id)
+    if not lead_id:
+        print('Erro ao cirar o lead')
+        return
+    
+    print('\n Lead Criado! Crie o contato com os dados solicitados abaixo:\n')
+    contact_data = contact_data_collect()
 
+    contact_response = handle_contact(contact_data, lead_id)
+    if contact_response:
+        print('Contato criado com sucesso!')
+    else:
+        print('Erro ao criar o contato')
 
-# Coletar e enviar Contato
-dados_contato = contact_data_collect()
-novo_contato = Contact(novo_lead.leadId, **dados_contato)
-contato_json = ContactSerializer.serialize(novo_contato)
-create_contact(contato_json)
+if __name__ == "__main__":
+    main()
