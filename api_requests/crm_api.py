ec38f777c8a0e9e1254ae607c8bad13b7b852ea4
Schlemper <schlemperdev@gmail.com>
2025-02-16 21:05:58 -0300

    added org API handle and refactored for error check

--- api_requests/crm_api.py
+++ api_requests/crm_api.py
@@ -7,26 +7,39 @@ load_dotenv()
 API_KEY = os.getenv("SPOTTER_API_KEY")
 BASE_URL = os.getenv("SSPOTER_BASE_URL")
 
-
-def create_lead(lead_json):
-    response = requests.post(f"{BASE_URL}/LeadsAdd",
-                             data=lead_json,
-                             headers={"Content-Type": "application/json",
-                                      "token_exact": f"{API_KEY}"})
-    return response.json()
-
-
-def create_contact(contact_json):
-    response = requests.post(f"{BASE_URL}/organizationAdd",
-                             data=contact_json,
-                             headers={"Content-Type": "application/json",
-                                      "token_exact": f"{API_KEY}"})
-    return response.json()
+endpoint = ""
+url = f"{BASE_URL}{endpoint}"
+headers = {"Content-Type": "application/json", "token_exact": f"{API_KEY}"}
 
 
 def create_org(org_json):
-    response = requests.post(f"{BASE_URL}/personsAdd",
-                             data=org_json,
-                             headers={"Content-Type": "application/json",
-                                      "token_exact": f"{API_KEY}"})
-    return response.json()
+    endpoint = "/personsAdd"
+    response = requests.post(url, data=org_json, headers=headers)
+    if response.status_code == 201:
+        return response.json()  # Retorna os dados da organização criada, incluindo o ID
+    else:
+        print(f"Erro ao criar organização: {response.status_code}")
+        print(response.text)
+        return None
+
+
+def create_lead(lead_json):
+    endpoint = "/LeadsAdd"
+    response = requests.post(url, data=lead_json, headers=headers)
+    if response.status_code == 201:
+        return response.json()  # Retorna os dados do lead criado, incluindo o ID
+    else:
+        print(f"Erro ao criar lead: {response.status_code}")
+        print(response.text)
+        return None
+
+
+def create_contact(contact_json):
+    endpoint = "/personsAdd"
+    response = requests.post(url, data=contact_json, headers=headers)
+    if response.status_code == 201:
+        return response.json()  # Retorna os dados do contato criado
+    else:
+        print(f"Erro ao criar lead: {response.status_code}")
+        print(response.text)
+        return None
