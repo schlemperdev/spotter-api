e902401e38ccff20c91b2741bf41934e51c8d1e5
Schlemper <schlemperdev@gmail.com>
2025-02-18 13:46:46 -0300

    bug fix: base data class frozen

--- models/base.py
+++ models/base.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 
 
-@dataclass(frozen=True)
+@dataclass
 class BaseEntity:
     cpfCnpj: str
     name: str
--- serializers/base_serializer.py
+++ serializers/base_serializer.py
@@ -1,18 +1,7 @@
 class BaseSerializer:
     """Base serializer that can automatically convert objects to JSON-compatible dictionaries."""
 
-    @staticmethod
-    def clean_dict(data):
-        """Removes None values from a dictionary."""
-        return {k: v for k, v in data.items() if v is not None}
-
     @classmethod
     def serialize(cls, obj, extra_data=None):
         """ Serializes an object by automatically filtering out None values. Supports additional static fields. """
-        data = cls.clean_dict(data)  # Remove None values
-
-        # Merge extra_data if provided
-        if extra_data:
-            data.update(extra_data)
-
-        return data
+        return {k: v for k, v in asdict(obj).items() if v is not None}
